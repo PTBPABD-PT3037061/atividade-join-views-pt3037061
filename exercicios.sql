99d5546110ab8a3a3704b1ccfa98d868941624ad
motarenan-art <mota.renan@aluno.ifsp.edu.br>
2026-03-15 20:36:11 -0300

    Reformat SQL queries and create comments

--- exercicios.sql
+++ exercicios.sql
@@ -1,7 +1,6 @@
 
 -- Questão 01. Crie uma relação a partir da união das tabelas student e takes.
 
--- Aqui foi só um join para unir as duas tabelas
 SELECT *
 FROM student s
 JOIN takes t ON s.ID = t.ID;
@@ -10,8 +9,6 @@ JOIN takes t ON s.ID = t.ID;
 -- Questão 2. Contar a quantidade de cursos realizados pelos alunos do departamento 
 -- de Civil Eng. Ordenar de maneira descendente a quantidade de cursos.
 
-
--- Aqui eu usei o COUNT para pegar a quantidade e o ORDER BY com o DESC para ordenar na ordem q foi solicitada
 SELECT 
     s.ID, 
     s.name, 
@@ -27,7 +24,6 @@ ORDER BY "Quantidade de cursos" DESC;
 
 -- Questão 3. Criar uma view chamada 'civil_eng_students' a partir da relação 
 -- construída na Questão 2.
--- Aqui eu só criei a view em cima do select da questão anterior
 
 CREATE VIEW civil_eng_students AS
 SELECT 
